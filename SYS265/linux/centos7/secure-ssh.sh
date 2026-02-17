4b7a678c225c325a117791e53501b3aa08766799
nicoleamaral37 <nicole.amaral@mymail.champlain.edu>
2026-02-17 23:49:11 +0000

    made passwordless

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo useradd -m -s bin/bash $1
+sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo passwd -d $1
 
-sudo mkdir -p /home/$1/.ssh
+sudo mkdir  /home/$1/.ssh
 
 #add public key locally
-sudo cp .~/Tech-Journal-NicoleAmaral/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+sudo cp /home/nicole/Tech-Journal-NicoleAmaral/SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
 
 #Perms
 #set owner to new user, not root
@@ -18,5 +19,5 @@ sudo chmod 600 /home/$1/.ssh/authorized_keys
 #Disable root ssh
 sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
-sudo systemctl restart sshd
+sudo systemctl restart ssh
 
