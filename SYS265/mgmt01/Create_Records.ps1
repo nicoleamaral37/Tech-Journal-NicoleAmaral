6a9862cf349ce8704cb6ed979e649d255a727d3a
nicoleamaral37 <nicole.amaral@mymail.champlain.edu>
2026-02-19 22:02:15 -0500

    script

---
+++ SYS265/mgmt01/Create_Records.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [string]$Name,
+    [string]$IPv4
+)
+
+Import-Module DnsServer
+
+$Zone = "nicole.local"
+$PtrZone = "5.0.10.in-addr.arpa"
+$LastOctet = $IPv4.split(".")[-1]
+$PtrName = "$Name.$Zone."
+
+Add-DnsServerResourceRecordA `
+    -ZoneName $Zone `
+    -Name $LastOctet `
+    -IPv4Address $IPv4
+
+write-host $LastOctet.$PtrName
+
+Add-DnsServerResourceRecordPtr `
+    -ZoneName $zone `
+    -Name $Name `
+    -PtrDomainName 
